090121ba862d0b7547bead9446df3d190fed82e6
Liam Walsh <liw6800@gmail.com>
2020-11-16 02:09:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) %2 = 0
-    return "O"
-  else
+  if turn_count(board) % 2 = 0
     return "X"
+  else
+    return "O"
   end
 end
