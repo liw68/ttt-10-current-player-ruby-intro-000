7cb2e7fca0f7a4a8e01cc8cf2f11c52a9de0819f
Liam Walsh <liw6800@gmail.com>
2020-11-16 01:58:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,10 @@
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
+counter = 0
 def turn_count(board)
   board.each do |board|
+
+    counter += 1
 end
 
 def current_player
